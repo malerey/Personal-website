c445eb6decbbd0dcb5753dbd7f9bd10641558215
Malena <reymalena@gmail.com>
2018-07-31 17:52:23 -0300

    please work

--- js/Personal.js
+++ js/Personal.js
@@ -2,7 +2,7 @@ var typed = $(".typed");
 
 $(function() {
   typed.typed({
-    strings: ["Malena Rey^1500", "a developer^1500"],
+    strings: ["Malena Rey^1800", "a developer^1500"],
     typeSpeed: 100,
     loop: true,
   });
